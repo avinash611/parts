8aa4c7e475dbb0c19d67cfd2faf7715b5c76e5dd
avinash611 <avinash.kumar611@gmail.com>
2017-09-29 21:11:31 +0530

    Customer Account

--- app/code/local/Global/Helper/controllers/CustomerController.php
+++ app/code/local/Global/Helper/controllers/CustomerController.php
@@ -10,13 +10,19 @@ class Global_Helper_CustomerController extends Mage_Core_Controller_Front_Action
     public function updateCustomerAction()
     {
         $data = $this->getRequest()->getParams();
-        $customerSessionID = Mage::getSingleton('customer/session')->getCustomer()->getId();
+        $session = Mage::getSingleton('customer/session');
+        $customerSessionID = $session->getCustomer()->getId();
         if($data)
         {
             $customer = Mage::getModel('customer/customer')->load($customerSessionID);
             $customer->setWebsiteId(Mage::app()->getWebsite()->getId());
             $customer->addData($data);
             $customer->save();
+            $session->addSuccess("Form successfully saved.");
+            $this->_redirectReferer();
+        }
+        else{
+            $session->addError("Something went wrong..");
             $this->_redirectReferer();
         }
     }
